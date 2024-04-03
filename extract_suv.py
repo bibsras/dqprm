1e4c2889fd3b7ac1f818c739a4773c56ad519278
nemo17 <nemo17@instnlab2.extra.cea.fr>
2024-04-03 10:11:10 +0200

    premier commit

---
+++ extract_suv.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+"""
+fichier = open("20200527152905.861802.ig.tum", "r")
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")
+args = parser.parse_args()
+print(args.echo)
+"""
+import re 
+fic = "20200527152905.861802.ig.tum"
+with open(fic,"r") as f:
+    lines = f.read()
+m=re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)",lines, re.M)
+d={el[0]: float (el[1]) for el in m}
+print(d)
