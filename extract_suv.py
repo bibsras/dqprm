db366c42da447b99c564d626264b02bdb9844194
bibsras <Vous@exemple.com>
2024-04-03 13:57:41 +0200

    lecture du nom de fichier d'entrer avec argparse

--- extract_suv.py
+++ extract_suv.py
@@ -15,8 +15,12 @@ print(args.echo)
 """
 import re 
 fic = "20200527152905.861802.ig.tum"
-with open(fic,"r") as f:
-    lines = f.read()
-m=re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)",lines, re.M)
-d={el[0]: float (el[1]) for el in m}
-print(m)
+def do_it():
+    with open(fic,"r") as f:
+        lines = f.read()
+    m=re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)",lines, re.M)
+    d={el[0]: float (el[1]) for el in m}
+    print(m)
+
+if __name__=="__main__":
+    do_it()
